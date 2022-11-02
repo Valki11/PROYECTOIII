a3220329dbc8b397be6058809c6df0270a5bea17
valki11 <keilaramirez4249@gmail.com>
2022-11-01 21:20:06 -0600

    cambio

--- proyecto3.py
+++ proyecto3.py
@@ -2,12 +2,15 @@
 from tkinter import*
 Base=Tk()
 #se coloca el titulo que aparecerá en la ventana.
-Base.title("Proyecto III")
+Base.title ("Proyecto III")
 #Le damos un tamaño estándar a la ventana 
 Base.geometry("500x480")
 #Le damos un color a la ventana, en este caso nos basamos en la tabla de colores html. 
 Base.config(background='#CEE3F6')
 #le cambiamos el icono, colocando la ruta donde se encuentra nuestra imagen
 Base.iconbitmap('C:\\Users\\Compu Fire\\Downloads\\Squirtle.ico')
-
+OurApp=Frame()
+OurApp.pack()
+OurApp.config(background='#F6CEF5')
+OurApp.config(width="500", height="480")   
 Base.mainloop()
