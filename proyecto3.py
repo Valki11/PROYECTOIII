eeada665171510edba8ae465c7ae6389e840704b
valki11 <keilaramirez4249@gmail.com>
2022-11-02 12:14:53 -0600

    cambio

--- proyecto3.py
+++ proyecto3.py
@@ -5,10 +5,14 @@
 @license GPL v3
 @date 2022/11/03
 """
-#importamos la librería tkinder , colocamos el asterisco para exportar todo el paquete.
-from tkinter import*
+#importamos la librería tkinder.
+from tkinter import *
+import tkinter as tk
+from tkinter import filedialog
+
+def app():
+    pass
 Base=Tk()
-from tkinter import ttk
 #se coloca el titulo que aparecerá en la ventana.
 Base.title ("Proyecto III")
 #Le damos un tamaño estándar a la ventana 
@@ -17,10 +21,13 @@ Base.geometry("500x480")
 Base.config(background='#A9E2F3')
 #le cambiamos el icono, colocando la ruta donde se encuentra nuestra imagen
 Base.iconbitmap('C:\\Users\\Compu Fire\\Downloads\\Squirtle.ico')
-OurApp=Frame()
-OurApp.pack()
-OurApp.config(background='#F5F6CE')
-OurApp.config(width="400", height="480")
-OurApp.config(cursor="heart")
-
+OurApp=Frame(Base)
+OurApp.pack(side="left", anchor="n")
+etiqueta1=Label(OurApp, text="Bienveni@, qué desea hacer?",background='#F5F6CE',font=("Segoe Script",12))
+etiqueta1.pack()
+Base.config(cursor="heart")
+def abrirArchivos():
+    archivo=filedialog.askopenfilename(title="ABRIR",initialdir="C:/", filetypes=(("Archivos pdf","*.pdf"),("Todos los Archivos","*.*"),("Archivos de texto","*.txt")))
+    print(archivo)
+Button(Base, text="abrir archivo",font=("Segoe Script",9),command=abrirArchivos).pack() 
 Base.mainloop()
