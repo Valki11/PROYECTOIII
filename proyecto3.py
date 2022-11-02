d862084651e9dee6c19a66b9c49f4b2cb054588d
valki11 <keilaramirez4249@gmail.com>
2022-11-01 20:42:30 -0600

    cambio

--- proyecto3.py
+++ proyecto3.py
@@ -7,7 +7,7 @@ Base.title("Proyecto III")
 Base.geometry("500x480")
 #Le damos un color a la ventana, en este caso nos basamos en la tabla de colores html. 
 Base.config(background='#CEE3F6')
-#le cambiamos el icono 
+#le cambiamos el icono, colocando la ruta donde se encuentra nuestra imagen
 Base.iconbitmap('C:\\Users\\Compu Fire\\Downloads\\Squirtle.ico')
 
 Base.mainloop()
