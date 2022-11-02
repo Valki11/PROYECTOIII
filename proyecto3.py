f796a8b4b337a35fa7e3bdd5c82d9033e01b242e
Valki11 <102394314+Valki11@users.noreply.github.com>
2022-11-01 20:13:40 -0600

    Add files via upload

---
+++ proyecto3.py
@@ -0,0 +1,10 @@
+#importamos la librería tkinder , colocamos el asterisco para exportar todo el paquete.
+from tkinter import*
+Base=Tk()
+#se coloca el titulo que aparecerá en la ventana.
+Base.title("Proyecto III")
+#Le damos un tamaño estándar a la ventana 
+Base.geometry("400x180")
+#Le damos un color a la ventana, en este caso nos basamos en la tabla de colores html. 
+Base.config(background='#CEE3F6')
+Base.mainloop()
