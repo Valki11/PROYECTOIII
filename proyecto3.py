2a9f0d664a7407b24b83567c66d302b8f44f0a0c
valki11 <keilaramirez4249@gmail.com>
2022-11-01 20:41:20 -0600

    cambio

--- proyecto3.py
+++ proyecto3.py
@@ -4,7 +4,10 @@ Base=Tk()
 #se coloca el titulo que aparecerá en la ventana.
 Base.title("Proyecto III")
 #Le damos un tamaño estándar a la ventana 
-Base.geometry("400x180")
+Base.geometry("500x480")
 #Le damos un color a la ventana, en este caso nos basamos en la tabla de colores html. 
 Base.config(background='#CEE3F6')
+#le cambiamos el icono 
+Base.iconbitmap('C:\\Users\\Compu Fire\\Downloads\\Squirtle.ico')
+
 Base.mainloop()
